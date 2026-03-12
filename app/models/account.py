b2ac9df101119d97e63c7408f86a84c1dd6892ac
pyPaulson <agyekumpaul07@gmail.com>
2026-03-12 12:23:50 +0000

    initial commit for account endpoints

---
+++ app/models/account.py
@@ -0,0 +1,31 @@
+import uuid
+from sqlalchemy import Column, String, ForeignKey, Numeric, Enum, Boolean
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from app.database.session import Base
+import enum
+
+
+class AccountType(str, enum.Enum):
+    FLEX = "flex"
+    EMERGENCY = "emergency"
+    GOAL = "goal"
+    LOCKED = "locked"
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+
+    account_type = Column(Enum(AccountType), nullable=False)
+
+    currency = Column(String, default="GHS")
+
+    balance = Column(Numeric(12, 2), default=0)
+
+    is_active = Column(Boolean, default=True)
+
+    user = relationship("User", back_populates="accounts")
--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Column, String, Boolean, DateTime, Enum, func, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import enum
 import uuid
 
 Base = declarative_base()
 
 
-# Optional: Enum for gender
 class GenderEnum(str, enum.Enum):
     male = "male"
     female = "female"
@@ -40,3 +40,5 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     last_login = Column(DateTime(timezone=True), nullable=True)
+
+    accounts = relationship("Account", back_populates="user")
